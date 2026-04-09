b77cd0d67e997c99400040fe7dfd2a007ba96088
Labibul Fikri <vickrykonco56@gmail.com>
2026-04-09 10:46:07 +0700

    bug config

--- application/config/config.php
+++ application/config/config.php
@@ -26,7 +26,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 // $config['base_url'] = 'http://localhost/surat/';
 // $config['base_url'] = 'https://assistdpbt.surabaya.go.id/v2/';
 
-$config['base_url'] = 'http://localhost/non-lit/';
+$config['base_url'] = 'https://assistdpbt.surabaya.go.id/nonlitigasi/';
 
 //$config['base_url'] = 'https://assistdpbt.surabaya.go.id/nonlitigasi/';
 // $config['base_url'] = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://" . $_SERVER['HTTP_HOST'] . str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
